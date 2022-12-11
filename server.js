09fe87baf63a44bd0f5926330265c047f2b4d0d9
Thanh Le <thanhlevan0203@gmail.com>
2022-12-10 23:57:36 -0800

    create router

--- server.js
+++ server.js
@@ -5,4 +5,6 @@ const PORT = process.env.PORT || 3500
 
 app.use('/', express.static(path.join(__dirname, '/public')))
 
+app.use('/', require('./routes/root'))
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
