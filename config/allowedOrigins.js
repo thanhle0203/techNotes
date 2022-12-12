0a99f671d7d460763ddd8986639951815c827b65
Thanh Le <thanhlevan0203@gmail.com>
2022-12-12 12:57:35 -0800

    installed middleware cors

---
+++ config/allowedOrigins.js
@@ -0,0 +1,7 @@
+const allowedOriins = [
+  'http://localhost:3000',
+  'https://www.dandrepairshop.com',
+  'https://dandrepairshop.com'
+]
+
+module.exports = allowedOriins
---
+++ config/corsOptions.js
@@ -0,0 +1,15 @@
+const allowedOriins = require('./allowedOrigins')
+
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (allowedOriins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+}
+
+module.exports = corsOptions 
