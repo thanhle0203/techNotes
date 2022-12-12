37978c0866e07585eb121676c00e40254c06d4be
Thanh Le <thanhlevan0203@gmail.com>
2022-12-11 16:25:02 -0800

    build middleware error

--- middleware/logger.js
+++ middleware/logger.js
@@ -23,4 +23,8 @@ const logEvents = async(message, logFileName) => {
 
 const logger = (req, res, next) => {
   logEvents(`${req.method}\t${req.url}\t${req.headers.origin}`, 'reqLog.log')
+  console.log(`${req.method} ${req.path}`)
+  next()
 }
+
+module.exports = { logEvents, logger }
