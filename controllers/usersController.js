209966ef999e5df5a1f4f3a7d1990a421d80954f
Thanh Le <thanhlevan0203@gmail.com>
2022-12-13 10:21:40 -0800

    create user controller

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,4 +1,42 @@
 const User = require('../models/User')
 const Note = require('../models/Note')
+const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcrypt')
+
+// @desc Get all users
+// @route GET /users
+// @access Private
+const getAllUsers = asyncHandler(async(req, res) => {
+  
+})
+
+// @desc Create all users
+// @route POST /users
+// @access Private
+const createNewlUser = asyncHandler(async(req, res) => {
+  
+})
+
+// @desc Update a user
+// @route PATCH /users
+// @access Private
+const updatelUser = asyncHandler(async(req, res) => {
+  
+})
+
+// @desc Delete a user
+// @route DELETE /users
+// @access Private
+const deleteUser = asyncHandler(async(req, res) => {
+  
+})
+
+module.exports = {
+  getAllUsers,
+  createNewlUser,
+  updatelUser,
+  deleteUser
+}
+
 
 
