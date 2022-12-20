b66775a1480ced9f607df290c96308ff006d2dd4
Thanh Le <thanhlevan0203@gmail.com>
2022-12-19 17:44:26 -0800

    create DashHeader content

--- src/components/DashHeader.js
+++ src/components/DashHeader.js
@@ -1,7 +1,21 @@
+import { Link } from "react-router-dom"
+
 const DashHeader = () => {
-    return (
-        <div>DashHeader</div>
+    const content = (
+        <header className="dash-header">
+            <div className="dash-header__container">
+                <Link to="/dash/notes">
+                    <h1 className="dash-header__title">teachNotes</h1>
+                </Link>
+                <nav className="dash-header__nav">
+                    {/* add nav buttons later */}
+                </nav>
+            </div>
+        </header>
     )
+
+    return content
+    
 }
 
 export default DashHeader
