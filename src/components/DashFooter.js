d58b4c59714b11f2b3d2bdb51a6838ffcbe2d69a
Thanh Le <thanhlevan0203@gmail.com>
2022-12-19 18:46:41 -0800

    Create DashFooter content

--- src/components/DashFooter.js
+++ src/components/DashFooter.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHouse } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate, useLocation } from 'react-router-dom'
 
 const DashFooter = () => {
+  const navigate = useNavigate()
+  const { pathname } = useLocation()
+
+  const onGoHomeClicked = () => navigate('/dash')
+
+  let goHomeButton = null
+  if (pathname !== '/dash') {
+    goHomeButton = (
+      <button 
+        className='dash-footer__button icon-button'
+        title='Home'
+        onClick={onGoHomeClicked}
+      />
+    )
+  }
   const content = (
-    <footer>
+    <footer className='dash-footer'>
         <p>Current User:</p>
         <p>Status:</p>
     </footer>
