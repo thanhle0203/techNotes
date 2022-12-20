bd54575d206e5c2ff2592251e5dfe5aa29ecf420
Thanh Le <thanhlevan0203@gmail.com>
2022-12-19 18:02:52 -0800

    Create DashFooter

---
+++ src/components/DashFooter.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const DashFooter = () => {
+  const content = (
+    <footer>
+        <p>Current User:</p>
+        <p>Status:</p>
+    </footer>
+  )
+
+  return content
+}
+
+export default DashFooter
--- src/components/DashLayout.js
+++ src/components/DashLayout.js
@@ -1,8 +1,10 @@
 import { Outlet } from 'react-router-dom'
+import DashHeader from './DashHeader'
 
 const DashLayout = () => {
   return (
     <>
+      <DashHeader />
       <div className='dash-container'>
         <Outlet />
       </div>
