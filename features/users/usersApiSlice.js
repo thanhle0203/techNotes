6ece3fe4da5ed45d114668952a9fe7a325633a50
Thanh Le <thanhlevan0203@gmail.com>
2022-12-27 21:58:20 -0800

    create User Adapter

--- features/users/usersApiSlice.js
+++ features/users/usersApiSlice.js
@@ -38,3 +38,24 @@ export const usersApiSlice = apiSlice.injectEndpoints({
     })
   })
 })
+
+export const {
+    useGetUsersQuery,
+} = usersApiSlice
+
+// returns the query result object
+export const selectUsersResult = usersApiSlice.endpoints.getUsers.select()
+
+// creates memoized selector
+const selectUsersData = createSelector(
+    selectUsersResult,
+    usersResult => usersResult.data // normalized state object with ids & entities
+)
+
+// getSelectors creates these selectors and we rename them with aliases using destructuring
+export const {
+    selectAll: selectAllUsers,
+    selectById: selectUserById,
+    selectIds: selectUserIds
+    // Pass in a selector that returns the users slice of state
+} = usersAdapter.getSelectors(state => selectUsersData(state) ?? initialState)
